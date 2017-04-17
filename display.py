5ca5b97cc59e0cc7eb3cb5ba82ef868a26916b8d
Tanmay <tanmay.jha1@gmail.com>
2017-04-17 15:31:24 +0530

    Timer printing issue solved and music tones added

---
+++ display.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import os
+import sys
+
+#os.system('clear')
+#print("Hello")
+for i in range(0,3):
+    sys.stdout.write('\r'+str(i))
+#    print("\r{}".format(i))
+    #sys.stdout.flush()
--- melodyCatcher.py
+++ melodyCatcher.py
@@ -1,7 +1,10 @@
-from time import time,sleep
-import RPi.GPIO as GPIO
-import timeit
+from __future__ import print_function
 import os
+import timeit
+from time import sleep
+import sys
+
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 button1=11
@@ -42,91 +45,94 @@ end_time=0
 
 #Is there no compile here?
 #Why is the function being called when i am try to build and run. The parameter code is below na :/ 
+print("Welcome to the game")
+x=input("Enter the number of players")
+noOfPlayers=[x]
+flag_music=("Enter 1 to start the game")
 
-print("Welcome to the Game")
-flag_music=input("Enter 1 to start the Game ")
-k=0
-try: 
-	while(1):
-		if(flag_music==1):
-			#play music code
-			print("In this game, you will have to press the exact buttons to produce the tone which was played")
-			flag_music=0
-			sleep(1)
-			start_time=timeit.default_timer()
-		
-#		timer_display(start_time)
-		
-		end_time=timeit.default_timer()
-		os.system('clear')
-		print("Time"+str(end_time-start_time))
-	
-		if(GPIO.input(button1)==0):
-			print ("Button 1 was pressed")
-			GPIO.output(LED2,0)
-			GPIO.output(LED3,0)
-			GPIO.output(LED4,0)
-			GPIO.output(LED5,0)
-			GPIO.output(LED1,1)
-			sleep(.25)
-			k=k+1
-		if(GPIO.input(button2)==0):
-			print ("Button 2 was pressed")
-			GPIO.output(LED1,0)
-			GPIO.output(LED3,0)
-			GPIO.output(LED4,0)
-			GPIO.output(LED5,0)
-			GPIO.output(LED2,1)
-			sleep(.25)
-			k=k+1
-		if(GPIO.input(button3)==0):
-			print ("Button 3 was pressed")
-			GPIO.output(LED1,0)
-			GPIO.output(LED2,0)
-			GPIO.output(LED4,0)
-			GPIO.output(LED5,0)
-			GPIO.output(LED3,1)
-			sleep(.25)
-			k=k+1
-		if(GPIO.input(button4)==0):
-			GPIO.output(LED1,0)
-			GPIO.output(LED2,0)
-			GPIO.output(LED3,0)
-			GPIO.output(LED5,0)
-			GPIO.output(LED4,1)
-			print ("Button 4 was pressed")
-			sleep(.25)
-			k=k+1
-		if(GPIO.input(button5)==0):
-			GPIO.output(LED1,0)
-			GPIO.output(LED2,0)
-			GPIO.output(LED3,0)
-			GPIO.output(LED4,0)
-			GPIO.output(LED5,1);
-			print ("Button 5 was pressed")
-			sleep(.25)
-			k=k+1
-		if(k==5):
-			break
-	#I will have an array. And i will compare a particular element of array 
-	#accodingly. If any element doesn't matches then an restart message will 
-	#pop up	
-		#if(completed on time logic):
-		#if(1):
-		#	print("Congratulation you completed ")
-		#	print("Your time is ")
-		#	print(time)
-	#	else:
-	#		print("BOO")
-	#		print("You failed. Please try again")
+for i in range(0,x-1,1):
+    k=0
+    try:
+        while(1):
+            if(flag_music==1):
+                #play music code
+                print("All the best Player {}".format(i))
+                flag_music=0
+                sleep(1)
+                start_time=timeit.default_timer()
+                #timer_display(start_time)
 
-except KeyboardInterrupt:
-	GPIO.cleanup()
+            end_time=timeit.default_timer()
+            os.system('clear')
+            print("Time")
+            sys.stdout.write('\r' + str(end_time - start_time))
 
-#def timer_display():
-#	end_time=timeit.default_timer()
-#	os.system('clear')
-#	print ("Time"+ str(end_time-start_time))
-#	return
+            if(GPIO.input(button1)==0):
+                print ("Button 1 was pressed")
+                GPIO.output(LED2,0)
+                GPIO.output(LED3,0)
+                GPIO.output(LED4,0)
+                GPIO.output(LED5,0)
+                GPIO.output(LED1,1)
+                sleep(.25)
+                k=k+1
+            if(GPIO.input(button2)==0):
+                print ("Button 2 was pressed")
+                GPIO.output(LED1,0)
+                GPIO.output(LED3,0)
+                GPIO.output(LED4,0)
+                GPIO.output(LED5,0)
+                GPIO.output(LED2,1)
+                sleep(.25)
+                k=k+1
+            if(GPIO.input(button3)==0):
+                print ("Button 3 was pressed")
+                GPIO.output(LED1,0)
+                GPIO.output(LED2,0)
+                GPIO.output(LED4,0)
+                GPIO.output(LED5,0)
+                GPIO.output(LED3,1)
+                sleep(.25)
+                k=k+1
+            if(GPIO.input(button4)==0):
+                GPIO.output(LED1,0)
+                GPIO.output(LED2,0)
+                GPIO.output(LED3,0)
+                GPIO.output(LED5,0)
+                GPIO.output(LED4,1)
+                print ("Button 4 was pressed")
+                sleep(.25)
+                k=k+1
+            if(GPIO.input(button5)==0):
+                GPIO.output(LED1,0)
+                GPIO.output(LED2,0)
+                GPIO.output(LED3,0)
+                GPIO.output(LED4,0)
+                GPIO.output(LED5,1)
+                print ("Button 5 was pressed")
+                sleep(.25)
+                k=k+1
+            if(k==5):
+                break
+        #I will have an array. And i will compare a particular element of array
+    	#accodingly. If any element doesn't matches then an restart message will
+    	#pop up
+    		#if(completed on time logic):
+    		#if(1):
+    		#	print("Congratulation you completed ")
+    		#	print("Your time is ")
+    		#	print(time)
+    	#	else:
+    	#		print("BOO")
+    	#		print("You failed. Please try again")
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+    #def timer_display():
+    #	end_time=timeit.default_timer()
+    #	os.system('clear')
+    #	print ("Time"+ str(end_time-start_time))
+    #	return
 	
 	
