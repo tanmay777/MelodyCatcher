cd242f329dddb83b23eb603715223087cc1dd834
Tanmay <tanmay.jha1@gmail.com>
2017-04-18 22:17:00 +0530

    Project completed for single player.Bugs need to be checked for multiple players

--- display.py
+++ display.py
@@ -1,4 +1,14 @@
 from __future__ import print_function
 
-var=["e","d#","e","d#","e","b","d","c","a"]
+import timeit
+import sys
+x=0
+'''while x<50000:
+    start_time=timeit.default_timer()
+    end_time=timeit.default_timer()
+    sys.stdout.write('\rTimer: ' + str(end_time - start_time))
+    x=x+1'''
+x=30.239345
+print("%.2f" %x)
+
 
--- melodyCatcher.py
+++ melodyCatcher.py
@@ -1,32 +1,26 @@
-import pygame
 from __future__ import print_function
+import pygame
 import os
 import timeit
 from time import sleep
 import sys
-
-pygame.mixer.init()
-pygame.mixer.music.load("myFile.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
-
-
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-button1=11
-button2=12
-button3=13
-button4=15
-button5=16
-button6=2 #To be added
+
+button1=38
+button2=11
+button3=12
+button4=13
+button5=15
+button6=16
 
 LED1=31
 LED2=32
 LED3=33
 LED4=35
 LED5=36
+LED6=37
 time=0
 
 GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
@@ -41,6 +35,7 @@ GPIO.setup(LED2,GPIO.OUT)
 GPIO.setup(LED3,GPIO.OUT)
 GPIO.setup(LED4,GPIO.OUT)
 GPIO.setup(LED5,GPIO.OUT)
+GPIO.setup(LED6,GPIO.OUT)
 
 flagMusic=0
 second=0
@@ -56,158 +51,185 @@ end_time=0
 
 #Is there no compile here?
 #Why is the function being called when i am try to build and run. The parameter code is below na :/ 
-print("Welcome to the game")
-x=input("Enter the number of players")
-noOfPlayers=[x]
+os.system('clear')
 
-print ("The music will be played in 3")
-sleep(1)
-print ("2")
-sleep(1)
-print ("1")
-sleep(1)
+print("Welcome to the game")
+x=input("Enter the number of players ")
+playersValue=[x]
+
+flag_music=input("Enter 1 to start the game ")
+
+if(flag_music):
+    print ("The music will be played in ")
+    '''TO BE ADDED LATER
+	for k in range(3,0,-1):
+                sys.stdout.write("\r"+"{0}".format(k)+" seconds")
+		sys.stdout.flush()
+#		print("{0}".format(k)+" seconds","\r")
+		sleep(1)
+		#sys.stdout.write('\r' + "{}".format(k)+" seconds")
+else:
+	sys.exit()
+'''
+
 tune=["e","d#","e","d#","e","b","d","c","a"]
 tuneNew=[]
+
+'''TO BE ADDED LATER
 pygame.mixer.init()
-pygame.mixer.music.load("/Resources/furliseMainTune.mp3")
+pygame.mixer.music.load("Resources/furliseMainTune.mp3")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
-print("Now you have to play this music using the butttons")
+'''
+print("\nNow you have to play this music using the butttons")
 print("The player who completes the fastest will win the game")
 print("All the best!")
-flag_music=("Enter 1 to start the game")
-for i in range(0,x-1,1):
+#ADD THE WAIT CODE HERE ALSO AS PUT IN MUSIC TO START SNIPPET
+print(x)
+for i in range(0,x,1):
+    flag_music=1
     k=0
+    os.system('clear') #Why isn't it clearing?
+    run=1
+
     try:
-        while(1):
+        while(run):
             if(flag_music==1):
                 #play music code
-                print("All the best Player {}".format(i))
+                print("\nAll the best Player {}".format(i+1))
                 flag_music=0
                 sleep(1)
                 start_time=timeit.default_timer()
                 #timer_display(start_time)
-            end_time=timeit.default_timer()
-            os.system('clear')
-            print("Time")
-            sys.stdout.write('\r' + str(end_time - start_time))
 
+            end_time=timeit.default_timer()
+            sys.stdout.write('\rTimer: %.2f' %str(end_time - start_time))
+            playersValue.append(str(round(end_time - start_time,2)))
             if(GPIO.input(button1)==0):
-                print ("Button 1 was pressed")
+#                print ("Button 1 was pressed")
                 GPIO.output(LED2,0)
                 GPIO.output(LED3,0)
                 GPIO.output(LED4,0)
                 GPIO.output(LED5,0)
+                GPIO.output(LED6,0)
                 GPIO.output(LED1,1)
                 k=k+1
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/ANote2(9).mp3")
+                pygame.mixer.music.load("Resources/ANote2(9).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
+#		print(tuneNew)
                 if(["e", "d#", "e", "d#", "e", "b", "d", "c"]==tuneNew):
-                    tuneNew[k]="a"
+                    tuneNew.append("a")
+                    run=0
+                    print(k)
                 else:
-                    break
+                    run=0
             if(GPIO.input(button2)==0):
-                print ("Button 2 was pressed")
+#               print ("Button 2 was pressed")
                 GPIO.output(LED1,0)
                 GPIO.output(LED3,0)
                 GPIO.output(LED4,0)
                 GPIO.output(LED5,0)
+                GPIO.output(LED6,0)
                 GPIO.output(LED2,1)
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/BNote2(6).mp3")
+                pygame.mixer.music.load("Resources/BNote2(6).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
                 k=k+1
                 if (["e", "d#", "e", "d#", "e"] == tuneNew):
-                    tuneNew[k] = "b"
-                else:
-                    break
+                    tuneNew.append("b")
+		else:
+			run=0
             if(GPIO.input(button3)==0):
-                print ("Button 3 was pressed")
+#                print ("Button 3 was pressed")
                 GPIO.output(LED1,0)
                 GPIO.output(LED2,0)
                 GPIO.output(LED4,0)
                 GPIO.output(LED5,0)
+                GPIO.output(LED6,0)
                 GPIO.output(LED3,1)
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/CNote(8).mp3")
+                pygame.mixer.music.load("Resources/CNote(8).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
                 k=k+1
                 if (["e", "d#", "e", "d#", "e", "b", "d"] == tuneNew):
-                    tuneNew[k] = "c"
-                else:
-                    break
+                    tuneNew.append("c")
+		else:
+			run=0
             if(GPIO.input(button4)==0):
                 GPIO.output(LED1,0)
                 GPIO.output(LED2,0)
                 GPIO.output(LED3,0)
                 GPIO.output(LED5,0)
+                GPIO.output(LED6,0)
                 GPIO.output(LED4,1)
-                print ("Button 4 was pressed")
+#                print ("Button 4 was pressed")
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/DNote(7).mp3")
+                pygame.mixer.music.load("Resources/Dnote(7).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
                 k=k+1
                 if (["e", "d#", "e", "d#", "e", "b"] == tuneNew):
-                    tuneNew[k] = "d"
+                    tuneNew.append("d")
+		else:
+			run=0
             if(GPIO.input(button5)==0):
                 GPIO.output(LED1,0)
                 GPIO.output(LED2,0)
                 GPIO.output(LED3,0)
                 GPIO.output(LED4,0)
+                GPIO.output(LED6,0)
                 GPIO.output(LED5,1)
-                print ("Button 5 was pressed")
+#                print ("Button 5 was pressed")
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/DSharpNote(2,4).mp3")
+                pygame.mixer.music.load("Resources/DSharpNote(2,4).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
                 k=k+1
-                if(["e", "d#", "e",] == tuneNew):
-                    tuneNew[k] = "d#"
+                if(["e", "d#", "e"] == tuneNew):
+                    tuneNew.append("d#")
                 elif (["e"] == tuneNew):
-                    tuneNew[k] = "d#"
+                    tuneNew.append("d#")
                 else:
-                    break
+                    run=0
             if (GPIO.input(button6) == 0):
-                print("Button 6 was pressed")
+#                print("Button 6 was pressed")
                 GPIO.output(LED1, 0)
                 GPIO.output(LED2, 0)
+                GPIO.output(LED3, 0)
                 GPIO.output(LED4, 0)
                 GPIO.output(LED5, 0)
-                GPIO.output(LED3, 1)
+                GPIO.output(LED6, 1)
                 sleep(.25)
                 pygame.mixer.init()
-                pygame.mixer.music.load("/Resources/ENote(1,3,5).mp3")
+                pygame.mixer.music.load("Resources/ENote(1,3,5).mp3")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy() == True:
                     continue
-                k = k + 1
+                k=k+1
                 if([]==tuneNew):
-                    tuneNew[k] = "e"
+                    tuneNew.append("e")
                 elif(["e","d#"]==tuneNew):
-                    tuneNew[k]= "e"
+                    tuneNew.append("e")
                 elif(["e", "d#", "e", "d#"]==tuneNew):
-                    tuneNew[k]="e"
+                    tuneNew.append("e")
                 else:
-                    break
-            if(k==6):
-                print("Congrats you completed")
-                break
+                    run=0
+			
         #I will have an array. And i will compare a particular element of array
     	#accodingly. If any element doesn't matches then an restart message will
     	#pop up
@@ -228,5 +250,17 @@ for i in range(0,x-1,1):
     #	os.system('clear')
     #	print ("Time"+ str(end_time-start_time))
     #	return
-	
-	
+
+if(k!=9):
+    print("Sorry you failed")
+#	print(k)
+else:
+    print("Congrats you completed it")
+
+lowest=playersValue[0]
+for i in range(0,x,1):
+    if(playersValue[i]<lowest):
+        lowest=playersValue[i]
+        bestplayer=i
+print ("Player"+i+1,"Won the game!")
+
